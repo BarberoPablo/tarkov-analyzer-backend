d7fe810adc9cbbb548c44d4d1d2b2e6afc2b9e04
Pablo Barbero <pablobarbero220@gmail.com>
2024-09-11 11:37:08 -0300

    vercel

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,11 @@
-import { FleaMarketItem } from "./types";
 import express from "express";
 import axios from "axios";
-
 const app = express();
-
-let itemsData: FleaMarketItem[] = [];
-
-const fetchItems = async (): Promise<void> => {
-  try {
-    const response = await axios.post("https://api.tarkov.dev/graphql", {
-      query: `
+let itemsData = [];
+const fetchItems = async () => {
+    try {
+        const response = await axios.post("https://api.tarkov.dev/graphql", {
+            query: `
         {
           items(lang: en) {
             id
@@ -21,29 +17,25 @@ const fetchItems = async (): Promise<void> => {
           }
         }
       `,
-    });
-
-    itemsData = response.data.data.items;
-    console.log(itemsData);
-  } catch (error) {
-    console.error("Error fetching items:", error);
-  }
+        });
+        itemsData = response.data.data.items;
+        console.log(itemsData);
+    }
+    catch (error) {
+        console.error("Error fetching items:", error);
+    }
 };
 // Llama a fetchItems al iniciar el servidor
 fetchItems();
-
 // Configura el intervalo para actualizar los datos cada 8 horas
 setInterval(fetchItems, 28800000);
-
 app.get("/api/items", (_, res) => {
-  res.json(itemsData);
+    res.json(itemsData);
 });
-
 if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
 }
-
 export default app;
