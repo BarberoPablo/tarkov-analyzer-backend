287736792c958dabe185c483d102c7a533f72054
Pablo Barbero <pablobarbero220@gmail.com>
2024-09-11 10:55:27 -0300

    vercel

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,8 @@ import { FleaMarketItem } from "./types";
 
 const app = express();
 
-// Variable para almacenar la respuesta
 let itemsData: FleaMarketItem[] = [];
 
-// Función para obtener datos de la API de Tarkov
 const fetchItems = async (): Promise<void> => {
   try {
     const response = await axios.post("https://api.tarkov.dev/graphql", {
@@ -32,7 +30,6 @@ const fetchItems = async (): Promise<void> => {
   }
 };
 
-// Ruta para obtener los datos
 app.get("/api/items", (req, res) => {
   res.json(itemsData);
 });
@@ -45,19 +42,7 @@ app.get("/", (req, res) => {
   res.json({ message: "Hello /" });
 });
 
-// Llama a fetchItems al iniciar el servidor
 fetchItems();
-
-// Configura el intervalo para actualizar los datos cada 8 horas
 setInterval(fetchItems, 28800000);
-
-// Exporta el manejador de solicitudes para Vercel
-
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
 
 export default app;
